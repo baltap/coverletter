6b05e0babde17d5588a96b9cdb73beaebb817212
Peter <peter@macbookpro.lan>
2026-01-26 17:15:58 +0100

    chore: update live clerk domain and deployment script

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,7 +4,7 @@ export default {
   providers: [
     // Uncomment this once you have set up a Clerk app
     {
-      domain: "https://happy-antelope-82.clerk.accounts.dev",
+      domain: "https://clerk.scribe.cv",
       applicationID: "convex",
     },
   ],
