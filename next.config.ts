8bc6a5120d00c3cdd811dedd8c3da7fba6edcf39
Peter <peter@macbookpro.lan>
2026-01-26 15:58:12 +0100

    seo: redirect digitalocean subdomain to scribe.cv

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'test-page-azl3x.ondigitalocean.app',
+          },
+        ],
+        destination: 'https://scribe.cv/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
